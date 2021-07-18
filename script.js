9b1ff8488cffd08c02118a48412ea3b3d64f95b9
imildositoe <imildositoe@gmail.com>
2021-07-18 22:02:21 +0200

    old boxes JS code removed

--- script.js
+++ script.js
@@ -15,29 +15,3 @@ function changeColor() {
 (function () {
     setInterval(changeColor, 10);
 })();
-
-
-let box = [];
-box = document.getElementsByClassName('box-one');
-
-caixa.style.display = 'flex';
-caixa.style.flexWrap = 'wrap';
-
-let boxOne = document.createElement('div');
-boxOne.style.width = '5%';
-boxOne.style.height = '10px';
-boxOne.style.backgroundColor = 'white';
-
-function changeColorBox() {
-    let a = caixa;
-    for (let i = 0; i < 10; i++) {
-        boxOne.style.backgroundColor = getColor();
-        caixa.appendChild(boxOne);
-        a.appendChild(boxOne);
-        a=boxOne;
-    }
-}
-
-(function () {
-    setInterval(changeColorBox, 50);
-})();
